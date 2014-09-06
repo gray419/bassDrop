9dc286960ac4411e7707db3adbacaa9870d040d0
Robert <gray419@gmail.com>
2014-09-06 17:34:33 -0400

    Added drop spinner + UI changes

--- app/src/main/java/com/graywolf/bassdrop/Fragments/PlayActivityFragment.java
+++ app/src/main/java/com/graywolf/bassdrop/Fragments/PlayActivityFragment.java
@@ -1,6 +1,7 @@
 package com.graywolf.bassdrop.Fragments;
 
 import android.app.Fragment;
+import android.content.Context;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
@@ -8,8 +9,10 @@ import android.os.SystemClock;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.daimajia.androidanimations.library.Techniques;
@@ -31,31 +34,32 @@ public class PlayActivityFragment extends Fragment{
     private TextView mTimerValue;
     private View mRootView;
     private AdView mBannerAdView;
+    private Spinner mDropSelectorSpinner;
+    private Context mContext;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
         View rootView = inflater.inflate(R.layout.fragment_play, container, false);
         mRootView = rootView;
+        mContext = getActivity().getApplicationContext();
 
-        mBannerAdView = new AdView(getActivity().getApplicationContext());
-        mBannerAdView.setAdSize(AdSize.BANNER);
-        mBannerAdView.setAdUnitId("ca-app-pub-6013564165619332/177915880");
+        initBannerAd();
 
-        LinearLayout linearLayout = (LinearLayout) rootView.findViewById(R.id.linearlayoutAd);
-        linearLayout.addView(mBannerAdView);
-
-        AdRequest adRequest = new AdRequest.Builder()
-                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
-                .addTestDevice("41A8C241CE3F27B022E69AE9F5E4625D")
-                .build();
-
-        // Start loading the ad in the background.
-        mBannerAdView.loadAd(adRequest);
-
-        mMediaPlayer = MediaPlayer.create(getActivity().getApplicationContext(), R.raw.main_build);
+        mMediaPlayer = MediaPlayer.create(mContext, R.raw.main_build);
         mMediaPlayer.setLooping(true);
         mMediaPlayer.start();
+        final Button dropTheBassButton = (Button) rootView.findViewById(R.id.bassButton);
+        final Spinner spinner = (Spinner) mRootView.findViewById(R.id.dropSelecterSpinner);
+
+        // Create an ArrayAdapter using the string array and a default spinner layout
+                ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(mContext,
+                        R.array.drop_array, R.layout.drop_spinner_item);
+        // Specify the layout to use when the list of choices appears
+                adapter.setDropDownViewResource(R.layout.spinner_drop_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
 
         final Button buildButton = (Button) rootView.findViewById(R.id.buildButton);
         buildButton.setOnClickListener(new View.OnClickListener() {
@@ -65,23 +69,28 @@ public class PlayActivityFragment extends Fragment{
                 mMediaPlayer.reset();
 
                 if(!mBuildUpStarted){
-                    mMediaPlayer = MediaPlayer.create(getActivity().getApplicationContext(), R.raw.build_up_1);
+                    mMediaPlayer = MediaPlayer.create(mContext, R.raw.build_up_1);
                     mMediaPlayer.setLooping(true);
                     mMediaPlayer.start();
                     mBuildUpStarted = true;
                     buildButton.setText("Not Yet!");
+
+                    //Show Bass Option
+                    dropTheBassButton.setVisibility(View.VISIBLE);
                 }
                 else if(mBuildUpStarted){
-                    mMediaPlayer = MediaPlayer.create(getActivity().getApplicationContext(), R.raw.main_build);
+                    mMediaPlayer = MediaPlayer.create(mContext, R.raw.main_build);
                     mMediaPlayer.setLooping(true);
                     mMediaPlayer.start();
                     mBuildUpStarted = false;
                     buildButton.setText("Build Up");
+
+                    //Hide Bass Button
+                    dropTheBassButton.setVisibility(View.INVISIBLE);
                 }
             }
         });
 
-        Button dropTheBassButton = (Button) rootView.findViewById(R.id.bassButton);
         dropTheBassButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -90,7 +99,28 @@ public class PlayActivityFragment extends Fragment{
 
                 mMediaPlayer.stop();
 
-                mMediaPlayer = MediaPlayer.create(getActivity().getApplicationContext(), R.raw.main_drop_lonley_island);
+                int selectedDrop = spinner.getSelectedItemPosition();
+                int songResourceId=0;
+
+                switch (selectedDrop){
+                    case 0:
+                        songResourceId = R.raw.main_drop_lonley_island;
+                        break;
+                    case 1:
+                        songResourceId = R.raw.drop_snake_lunatic;
+                        break;
+                    case 2:
+                        songResourceId = R.raw.drop_salto_wiwek_onyourmark;
+                        break;
+                    case 3:
+                        songResourceId = R.raw.drop_brillz_swoop;
+                        break;
+                    case 4:
+                        songResourceId = R.raw.carnage_drop;
+                        break;
+                }
+
+                mMediaPlayer = MediaPlayer.create(mContext, songResourceId);
                 mMediaPlayer.start();
 
                 timeSwapBuff += timeInMilliseconds;
@@ -110,6 +140,8 @@ public class PlayActivityFragment extends Fragment{
         return rootView;
     }
 
+
+
     @Override
     public void onPause(){
         super.onPause();
@@ -122,6 +154,23 @@ public class PlayActivityFragment extends Fragment{
         super.onStop();
 
         mMediaPlayer.release();
+    }
+
+    private void initBannerAd() {
+        mBannerAdView = new AdView(mContext);
+        mBannerAdView.setAdSize(AdSize.BANNER);
+        mBannerAdView.setAdUnitId("ca-app-pub-6013564165619332/177915880");
+
+        LinearLayout linearLayout = (LinearLayout) mRootView.findViewById(R.id.linearlayoutAd);
+        linearLayout.addView(mBannerAdView);
+
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .addTestDevice("41A8C241CE3F27B022E69AE9F5E4625D")
+                .build();
+
+        // Start loading the ad in the background.
+        mBannerAdView.loadAd(adRequest);
     }
 
     private Runnable updateTimerThread = new Runnable() {
